e5cbaf5c7025c5371fafaa57ad93de95d95e7add
haberkern <luke.haberkern@colorado.edu>
2013-06-13 17:02:38 -0500

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,4 +1,6 @@
-#mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0  -r 320x240 -f 28" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
-#mjpg_streamer -i "/usr/lib/input_uvc.so  -d /dev/video0  -r 640x360 -f 60" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
-mjpg_streamer -i "/usr/lib/input_uvc.so  -d /dev/video0  -r 320x240 -q 50 -f 30" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
+#mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 320x240 -f 28" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
+#mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 640x360 -f 60" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
+#mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 320x240 -q 50 -f 30" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
+mjpg_streamer -i "/usr/lib/input_uvc.so -d /dev/video0 -r 640x320 -q 50 -f 15" -o "/usr/lib/output_http.so -p 8090 -w /var/www"
+
 
